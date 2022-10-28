5ead1f627c033945a038f2b7ebbcb778454602dc
julian.lang <julian.lang@centigrade.de>
2022-10-28 13:57:52 +0200

    feat: add log helper

---
+++ src/helper.ts
@@ -0,0 +1,8 @@
+import { Ref } from "./types";
+import { createExtension } from "./utility";
+
+export const log = (output?: Ref<any[]>) =>
+  createExtension((value) => {
+    const additionalOutput = output?.() ?? [];
+    console.log(value, ...additionalOutput);
+  });
