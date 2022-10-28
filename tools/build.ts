baae66eac17f74f2269ab0242c7cfaae28262b59
julian.lang <julian.lang@centigrade.de>
2022-10-28 12:22:26 +0200

    add build script

---
+++ tools/build.ts
@@ -0,0 +1,83 @@
+import { build, BuildOptions } from "esbuild";
+import { outputFile } from "fs-extra";
+import { resolve } from "path";
+import { cwd } from "process";
+import * as packageJson from "../package.json";
+/*  
+    shx cp package.json dist/package.json
+    shx cp README.md dist/README.md
+    node buildhelper/package-json-preparer.js package.json dist/package.json
+*/
+const cwdPath = (path: string) => {
+  return resolve(cwd(), path);
+};
+
+class Program {
+  static async main(production: boolean) {
+    const commonBuildOpts: BuildOptions = {
+      entryPoints: [cwdPath("./src/index.ts")],
+      platform: "node",
+      target: "es2020",
+      bundle: true,
+      minify: production,
+    };
+
+    await build({
+      ...commonBuildOpts,
+      format: "esm",
+      outfile: cwdPath("./dist/mjs/index.js"),
+      tsconfig: cwdPath("./tsconfig-mjs.json"),
+    });
+    await build({
+      ...commonBuildOpts,
+      format: "cjs",
+      outfile: cwdPath("./dist/cjs/index.js"),
+      tsconfig: cwdPath("./tsconfig-cjs.json"),
+    });
+
+    await this.preparePackageJson();
+  }
+
+  private static async preparePackageJson() {
+    const { name, version, author, description, keywords, license, main } =
+      packageJson;
+
+    const marketing = {
+      homepage: "",
+      repository: {
+        type: "git",
+        url: "https://github.com/Centigrade/ngtx.git",
+      },
+    };
+    const logistics = {
+      files: "**/*",
+      main: "./cjs/index.js",
+      module: "./mjs/index.js",
+      exports: {
+        ".": {
+          import: "./mjs/index.js",
+          require: "./cjs/index.js",
+        },
+      },
+    };
+    const output = {
+      name,
+      version,
+      author,
+      description,
+      keywords,
+      license,
+      ...marketing,
+      ...logistics,
+    };
+
+    await outputFile(cwdPath("./dist/package.json"), JSON.stringify(output));
+  }
+}
+
+const args = process.argv.slice(1);
+const production = args.includes("--prod");
+
+Program.main(production)
+  .catch(console.error)
+  .then(() => console.log("Built."));
