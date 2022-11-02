c95beef94bea8daf4c3a3364647bf192b5fcd36b
julian.lang <julian.lang@centigrade.de>
2022-11-02 12:48:06 +0100

    feat: improve helpers

--- src/helper.ts
+++ src/helper.ts
@@ -4,14 +4,20 @@ import { createExtension } from "./utility";
 export const log = (output?: Ref<any[]>) =>
   createExtension((value) => {
     const additionalOutput = output?.() ?? [];
-    console.log(value, ...additionalOutput);
+    output ? console.log(...additionalOutput) : console.log(value);
   });
 
 export function the<T, K extends keyof T>(value: T): () => T;
 export function the<T, K extends keyof T>(value: T, access: K): () => T[K];
-export function the<T, K extends keyof T>(
+export function the<T, R>(value: T, access: (value: T) => R): () => R;
+export function the<T, K extends keyof T, R>(
   value: T,
-  access?: K
-): () => T | T[K] {
-  return () => (access ? value[access] : value);
+  access?: K | ((value: T) => R)
+): () => T | T[K] | R {
+  return () => {
+    if (access == null) return value;
+    else {
+      return typeof access === "string" ? value[access] : value;
+    }
+  };
 }
