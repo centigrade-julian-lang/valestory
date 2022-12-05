0971d188bdd14807a8f7eba31f416fc94724e5f3
julian.lang <julian.lang@centigrade.de>
2022-12-05 18:55:37 +0100

    chore: format code

--- src/extensions/expectations/equal.ts
+++ src/extensions/expectations/equal.ts
@@ -10,7 +10,9 @@ export interface NumberComparisonOpts {
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface ComparisonOpts extends NumberComparisonOpts {}
 
-export function equal<T, I extends T = T>(expected: Partial<I>): Extension<T>;
+export function equal<T, I extends T = T>(
+  expected: Partial<I> | I
+): Extension<T>;
 export function equal<T extends Record<never, unknown>>(
   expected: Record<string, any>
 ): Extension<T | undefined | null>;
